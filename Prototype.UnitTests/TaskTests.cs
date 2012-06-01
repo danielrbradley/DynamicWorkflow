c5787a05d7aecc69003e7f19bb99e45436381165
daniel <daniel@danielbradley.net>
2012-06-01 16:52:52 +0100

    Create task unit tests.
    Known issue: workflow resuming not implemented so create task test will fail.

---
+++ Prototype.UnitTests/TaskTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DynamicWorkflow.Prototype.UnitTests
+{
+    [TestClass]
+    public class TaskTests
+    {
+        public const string DefaultWorkflowName = "Test Workflow";
+        public const string DefaultTaskName = "Test Task";
+        public const string DefaultQueueName = "Test Queue";
+        private Workflow defaultWorkflow;
+        private Queue defaultQueue;
+        private Database database;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            this.database = new Database();
+            Workflow.Create(database, DefaultWorkflowName);
+            Queue.Create(database, DefaultQueueName);
+            defaultWorkflow = Workflow.Get(database, DefaultWorkflowName);
+            defaultQueue = Queue.Get(database, DefaultQueueName);
+        }
+
+        [TestMethod]
+        public void CreateTaskInSuspension()
+        {
+            Task.Create(database, DefaultWorkflowName, DefaultTaskName, DefaultQueueName);
+            Assert.IsTrue(defaultWorkflow.TaskNames.ContainsKey(DefaultTaskName));
+            Assert.IsTrue(defaultWorkflow.Tasks.ContainsKey(defaultWorkflow.TaskNames[DefaultTaskName]));
+            Assert.AreEqual(0, defaultQueue.QueuedTasks.Count);
+            Assert.AreEqual(defaultWorkflow.TaskNames[DefaultTaskName], defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].Id);
+            Assert.AreEqual(DefaultTaskName, defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].Name);
+            Assert.AreEqual(TaskState.Queued, defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].State);
+            Assert.AreEqual(defaultQueue.Id, defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].QueueId);
+            Assert.IsNotNull(defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].DependancyTo);
+            Assert.IsNotNull(defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].DependantOn);
+            Assert.IsNotNull(defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].OutstandingDependencies);
+        }
+
+        [TestMethod]
+        public void CreateTask()
+        {
+            Workflow.Resume(database, DefaultWorkflowName);
+            Task.Create(database, DefaultWorkflowName, DefaultTaskName, DefaultQueueName);
+            Assert.IsTrue(defaultWorkflow.TaskNames.ContainsKey(DefaultTaskName));
+            Assert.IsTrue(defaultWorkflow.Tasks.ContainsKey(defaultWorkflow.TaskNames[DefaultTaskName]));
+            Assert.AreEqual(1, defaultQueue.QueuedTasks.Count);
+            Assert.AreEqual(defaultWorkflow.TaskNames[DefaultTaskName], defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].Id);
+            Assert.AreEqual(DefaultTaskName, defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].Name);
+            Assert.AreEqual(TaskState.Queued, defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].State);
+            Assert.AreEqual(defaultQueue.Id, defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].QueueId);
+            Assert.IsNotNull(defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].DependancyTo);
+            Assert.IsNotNull(defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].DependantOn);
+            Assert.IsNotNull(defaultWorkflow.Tasks[defaultWorkflow.TaskNames[DefaultTaskName]].OutstandingDependencies);
+        }
+
+        [TestMethod]
+        public void GetTask()
+        {
+            CreateTaskInSuspension();
+            var task = Task.Get(database, DefaultWorkflowName, DefaultTaskName);
+            Assert.AreEqual(DefaultTaskName, task.Name);
+        }
+    }
+}
