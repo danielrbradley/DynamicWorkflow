b89f2b9e7959f590d5f3b975458e87c198b247fe
daniel <daniel@danielbradley.net>
2012-06-01 16:20:36 +0100

    Improve workflow create asserts.

--- Prototype.UnitTests/WorkflowTests.cs
+++ Prototype.UnitTests/WorkflowTests.cs
@@ -24,7 +24,13 @@ namespace DynamicWorkflow.Prototype.UnitTests
             Workflow.Create(database, DefaultWorkflowName);
             Assert.IsTrue(database.WorkflowNames.ContainsKey(DefaultWorkflowName));
             Assert.IsTrue(database.Workflows.ContainsKey(database.WorkflowNames[DefaultWorkflowName]));
+            Assert.AreEqual(database.WorkflowNames[DefaultWorkflowName], database.Workflows[database.WorkflowNames[DefaultWorkflowName]].Id);
             Assert.AreEqual(DefaultWorkflowName, database.Workflows[database.WorkflowNames[DefaultWorkflowName]].Name);
+            Assert.IsTrue(database.Workflows[database.WorkflowNames[DefaultWorkflowName]].IsSuspended);
+            Assert.IsNotNull(database.Workflows[database.WorkflowNames[DefaultWorkflowName]].CompletedTasks);
+            Assert.IsNotNull(database.Workflows[database.WorkflowNames[DefaultWorkflowName]].TaskNames);
+            Assert.IsNotNull(database.Workflows[database.WorkflowNames[DefaultWorkflowName]].Tasks);
+            Assert.IsNotNull(database.Workflows[database.WorkflowNames[DefaultWorkflowName]].WorkflowLock);
         }
 
         [TestMethod]
@@ -33,7 +39,6 @@ namespace DynamicWorkflow.Prototype.UnitTests
             CreateWorkflow();
             var workflow = Workflow.Get(database, DefaultWorkflowName);
             Assert.AreEqual(DefaultWorkflowName, workflow.Name);
-            Assert.IsTrue(workflow.IsSuspended);
         }
 
         [TestMethod]
